db0280935395606e7804f0d9aff8c65ee02f47c1
杨三宁 <ysn_2020@163.com>
2020-04-27 11:16:01 +0800

    [ +] 初始化仓库

---
+++ app/src/main/java/ysn/com/demo/behavior/BehaviorActivity.java
@@ -0,0 +1,21 @@
+package ysn.com.demo.behavior;
+
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+public class BehaviorActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_behavior);
+    }
+}
